c713e1b084de302b4bab79d290b67207b6838951
Isaac Hancock <calmlakes7@gmail.com>
2023-02-22 12:00:46 -0500

    pushing to check on grendel

--- projects/proj2/renamer.cc
+++ projects/proj2/renamer.cc
@@ -418,6 +418,7 @@ bool renamer::precommit(bool &completed,
                     bool &exception, bool &load_viol, bool &br_misp, bool &val_misp,
                 bool &load, bool &store, bool &branch, bool &amo, bool &csr,
             uint64_t &PC){
+    print("test1");
     if (!AL->empty()){
         active_list_entry * entry = AL->at(0);
 
@@ -439,7 +440,7 @@ bool renamer::precommit(bool &completed,
     else {
         return false;
     }
-
+     print("test2");
     
 }
 
